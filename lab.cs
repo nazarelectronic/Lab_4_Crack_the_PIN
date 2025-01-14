c72e3b3971f36b5cdf330083e39b0e50711d66df
nazarelectronic <125883505+nazarelectronic@users.noreply.github.com>
2025-01-14 13:34:30 +0200

    Create lab.cs

---
+++ lab.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program {
+    static void Main() {
+        while (true) {
+            Console.Write("Введiть хеш: ");
+            string Hash = Console.ReadLine(); 
+
+            bool isFound = false; 
+            for (int i = 0; i <= 99999; i++) {
+                string password = i.ToString("D5");
+                string hash = MD5Hash(password);
+
+                if (hash == Hash) {
+                    Console.WriteLine($"Пароль: {password}");
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (!isFound) {
+                Console.WriteLine("Пароль не знайдено.");
+            } 
+            Console.WriteLine();
+        }
+    }
+
+    static string MD5Hash(string input) {
+        using (MD5 md5 = MD5.Create()) {
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in hashBytes) {
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
